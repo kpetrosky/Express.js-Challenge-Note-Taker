307ca09be5a5e747c80585f7c817eb73daadb6a4
kpetrosky <kpetrosky36@gmail.com>
2023-04-13 18:06:59 -0700

    continued to work on get and post code

--- server.js
+++ server.js
@@ -57,28 +57,9 @@ app.post('/api/notes', (req, res) => {
     })
   });
   
-//   // POST Route for a new UX/UI tip
-//   app.post('/api/tips', (req, res) => {
-//     console.info(`${req.method} request received to add a tip`);
-  
-//     const { username, topic, tip } = req.body;
-  
-//     if (req.body) {
-//       const newTip = {
-//         username,
-//         tip,
-//         topic,
-//         tip_id: uuid(),
-//       };
-  
-//       readAndAppend(newTip, './db/tips.json');
-//       res.json(`Tip added successfully 🚀`);
-//     } else {
-//       res.error('Error in adding tip');
-//     }
-//   });
+//similar to above but with delete stuff
 
-// GET Route for homepage
+
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
